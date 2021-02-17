de78f7a8674484cb634a6e0879a7d158a99428e2
Szelma139 <raduniek1@gmail.com>
2021-02-17 21:04:05 +0100

    d

---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './WelcomePage.css';
+
+export const WelcomePage = () => {
+    return (
+      <>
+      <div className="header-text">
+      <h1>1000 words challenge</h1>
+      </div>
+        <div className="welcome-page">
+          <a href="" className="btn-grad" >
+            <i></i> No account
+          </a>
+
+          <a href="" className="btn-grad">
+            <i></i> I want to login
+          </a>
+
+        </div>
+
+        </>
+    )
+}
